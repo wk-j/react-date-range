ca84718172ec6627d141a774c0d3f7ee1109366c
wk-j <somnuk.wk@gmail.com>
2019-06-04 16:39:49 +0700

    Locale

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,51 @@
 import React from "react";
 import { render } from "react-dom"
 
-export class App extends React.Component<{}, {}> {
+import 'react-date-range/dist/styles.css' // main style file
+import 'react-date-range/dist/theme/default.css' // theme css file
+import { DateRangePicker, DateRange } from "react-date-range";
+
+import locale from 'date-fns/locale/th';
+
+type Range = {
+    startDate: Date
+    endDate: Date
+    key: string
+}
+
+type State = {
+    ranges: Range[]
+}
+
+export class App extends React.Component<{}, State> {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            ranges: [
+                {
+                    startDate: new Date(),
+                    endDate: new Date(),
+                    key: "selection"
+                }
+            ]
+        }
+    }
+
+    handleSelect = (ranges) => {
+        this.setState({ ranges: [ranges.selection] })
+    }
 
     render() {
         return (
-            <h1>Hello</h1>
+            <DateRange
+                locale={locale}
+                rangedCalendars={false}
+                ranges={this.state.ranges}
+                onChange={this.handleSelect}
+            />
         )
     }
-
 }
 
 render(<App />, document.getElementById("root"))
